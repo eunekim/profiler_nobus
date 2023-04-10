ed0e9fe836ccb6973f7ae40a6f4120ad64ca3814
eunekim <keh8992@gmail.com>
2023-04-10 16:43:32 +0900

    수정

--- minmaxavg.js
+++ minmaxavg.js
@@ -1,6 +1,5 @@
-function a(core) {
+function a() {
     var min = core[0];
-
     for (var i = 0; i < core.length; i++) {
         
         if (core[i] < min) {
@@ -11,9 +10,8 @@ function a(core) {
 };
 
 
-function b(core) {
+function b() {
     var max = 0;
-
     for (var i = 0; i < core.length; i++) {
         if (core[i] > max) {
             max = core[i];
@@ -22,7 +20,7 @@ function b(core) {
     return max;
 };
 
-function c (core){ 
+function c (){ 
     var sum = 0;   
     for (var i = 0; i < core.length; i++) {
         sum = sum + core[i];
@@ -31,14 +29,21 @@ function c (core){
     return avg;
 };
 
-const aaa = [1,2,3];
-console.log(Math.max(...aaa));
+//const aaa = [1,2,3];
+//console.log(Math.max(...aaa));
 
-let core = [1,2,3,4,5];
-console.log(a(core));
-console.log(b(core));
-console.log(c(core));
+//let core = [1,2,3,4,5];
 
-function dbsubmit(){
-    alert("dkdk")
-};
+const core = document.querySelector('#inputFile');
+core.addEventListener("change", output);
+
+function output(){
+    const file = new FileReader();
+
+    file.readAsText(this.files[0]);
+    a(file);
+    b(file);
+    c(file);
+
+    document.write(min, max, avg);
+}
