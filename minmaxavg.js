6d37432d6c5af72c1b42bb8ea830351357fd0bae
eunekim <keh8992@gmail.com>
2023-04-10 17:46:22 +0900

    수정2

--- minmaxavg.js
+++ minmaxavg.js
@@ -34,10 +34,27 @@ function c (){
 
 //let core = [1,2,3,4,5];
 
-const core = document.querySelector('#inputFile');
-core.addEventListener("change", output);
+const evnet_a = document.querySelector('#inputFile');
+evnet_a.addEventListener('change', function(event){
 
-function output(){
+    var fileList = evnet_a.files || event.target.files;
+
+    var file = fileList[0];
+    var reader = new FileReader();
+
+    reader.onload = function(progressEvent){
+        console.log(progressEvent.target.result);
+    };
+    reader.readAsText(file);
+    const valueA = reader.readAsArrayBuffer(file);
+
+    a(valueA);
+    b(valueA);
+    c(valueA);
+    
+});
+
+/* function output(){
     const file = new FileReader();
 
     file.readAsText(this.files[0]);
@@ -46,4 +63,6 @@ function output(){
     c(file);
 
     document.write(min, max, avg);
-}
+
+    document.write("hellos");
+}; */
